e60d84dc5e6570c966b4fbe3756736b814742927
Kevin López Guzmán <k.lopezguzman@ugto.mx>
2021-12-03 17:07:03 -0600

    index.js
    se modificó los archivos sigup y signin

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,23 @@ const {Router} = require('express');
 const router = Router();
 
 router.get('/',(req,res)=>{
-    console.log('Página de inicio');
+    console.log('HOME');
+});
+
+router.get('/signup',(req, res, next) => {
+
+});
+
+router.post('/signup',(req, res, next) => {
+    
+});
+
+router.get('/signin',(req, res, next) => {
+
+});
+
+router.post('/signin',(req, res, next) => {
+    
 });
 
 module.exports = router;
