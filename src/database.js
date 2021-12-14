e4bdca2cd6dab531b78a55826ec7e50fd15f3086
Kevin López Guzmán <k.lopezguzman@ugto.mx>
2021-12-14 14:57:38 -0600

    database
    configuración de database, se modificó mensaje "el hurón llegó a la estación"

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
-//cadena de conexión
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://mongo/inventario') // necesitamos un nombre para base de datos, en este caso usamos inentario, se usa mongo por que ese l nombre que le dimos a nuestro contenedor
-.then(db => console.log('EL HURÓN LLEGÓ A LA ESTACIÓN',db.connection.host)) //mensaje para avisar que la base de datos se conectó
-.catch(err => console.error(err)); // mensaje en caso que la base datos no se conectara (esperemos que no)
+mongoose.connect('mongodb://mongo/proyecto')
+    .then(db => console.log('EL HURÓN LLEGÓ A LA ESTACIÓN', db.connection.host))
+    .catch(err => console.error(err));
+
